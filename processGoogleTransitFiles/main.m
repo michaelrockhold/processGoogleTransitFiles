2fbc9f33e34b1330b2eaf3b3ccd1ed19086c4ea4
Michael Rockhold <michael@Zephyr.local>
2011-07-18 20:41:56 -0700

    first running build

--- processGoogleTransitFiles/main.m
+++ processGoogleTransitFiles/main.m
@@ -3,30 +3,100 @@
 //  processGoogleTransitFiles
 //
 //  Created by Michael Rockhold on 7/17/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 The Rockhold Company. All rights reserved.
 //
+
 
 NSManagedObjectModel *managedObjectModel();
 NSManagedObjectContext *managedObjectContext();
 
+NSURL* g_dbURL = nil;
+NSString* g_storeType = nil;
+
 int main (int argc, const char * argv[])
 {
+    int exitCode = 0;
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    NSArray* args = [[NSProcessInfo processInfo] arguments];
+    if ( [args count] < 3 )
+    {
+        NSLog(@"Error not enough arguments");
+        exitCode = 2; goto exit;
+    }
+    
+    NSString* dbPath = [args objectAtIndex:1];
+    NSString* googleTransitDirPath = [args objectAtIndex:2];
+    
+    if ( [dbPath hasSuffix:@".sqlite"] )
+    {
+        g_storeType = NSSQLiteStoreType;
+    }
+    else if ( [dbPath hasSuffix:@".bin"] )
+    {
+        g_storeType = NSBinaryStoreType;
+    }
+    else if ( [dbPath hasSuffix:@".xml"] )
+    {
+        g_storeType = NSXMLStoreType;
+    }
+    else
+    {
+        NSLog(@"Error: arg 1 (%@) should be a file name ending with .sqlite, .bin, or .xml", dbPath);
+        exitCode = 3; goto exit;
+    }
+    g_dbURL = [NSURL fileURLWithPath:dbPath];
+    
+    // check that the googleTransitDirPath actually exists and contains all the required files
+    BOOL isDir = NO;
+    NSFileManager *fileManager = [[[NSFileManager alloc] init] autorelease];
+    
+    if ( [googleTransitDirPath hasPrefix:@"~"] )
+        googleTransitDirPath = [googleTransitDirPath stringByExpandingTildeInPath];
+    
+    if ( !([fileManager fileExistsAtPath:googleTransitDirPath isDirectory:&isDir] && isDir) )
+    {
+        NSLog(@"Error: arg 2 must be a directory containing a Google Transit database");
+        exitCode = 5; goto exit;
+    }
+    
+    static NSString* gtFiles[] = {
+        @"agency.txt", @"pattern_pairs.txt", @"stops.txt", @"calendar_dates.txt", 
+        @"routes.txt", @"trips.txt", @"fare_attributes.txt", @"shapes.txt", 
+        @"fare_rules.txt", @"stop_times.txt",
+        NULL
+    };
+    
+    NSString** gtFile = gtFiles;
+    while ( *gtFile )
+    {
+        if ( ![fileManager fileExistsAtPath:[googleTransitDirPath stringByAppendingPathComponent:*gtFile]] )
+            break;
+        gtFile++;
+    }
+    if ( *gtFile != NULL )
+    {
+        NSLog(@"Error: required Google Transit file %@ is missing from input directory", *gtFile);
+        exitCode = 5; goto exit;
+    }
+    
+    // Create the managed object context
+    NSManagedObjectContext *context = managedObjectContext();
 
-        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-        // Create the managed object context
-        NSManagedObjectContext *context = managedObjectContext();
-        
-        // Custom code here...
-        // Save the managed object context
-        NSError *error = nil;    
-        if (![context save:&error]) {
-            NSLog(@"Error while saving %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
-            [pool drain]; 
-            exit(1);
-        }
-        [pool drain];
-    return 0;
+    // Custom code here...
+    
+    
+    // Save the managed object context
+    NSError *error = nil;    
+    if ( ![context save:&error] )
+    {
+        NSLog(@"Error while saving %@", ([error localizedDescription] != nil) ? [error localizedDescription] : @"Unknown Error");
+        exitCode = 1;
+        goto exit;
+    }
+exit:
+    [pool drain];
+    return exitCode;
 }
 
 NSManagedObjectModel *managedObjectModel() {
@@ -39,7 +109,7 @@ NSManagedObjectModel *managedObjectModel() {
     
     NSString *path = [[[NSProcessInfo processInfo] arguments] objectAtIndex:0];
     path = [path stringByDeletingPathExtension];
-    NSURL *modelURL = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"mom"]];
+    NSURL *modelURL = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"momd"]];
     model = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
     
     return model;
@@ -48,7 +118,8 @@ NSManagedObjectModel *managedObjectModel() {
 NSManagedObjectContext *managedObjectContext() {
 
     static NSManagedObjectContext *context = nil;
-    if (context != nil) {
+    if (context != nil)
+    {
         return context;
     }
 
@@ -58,16 +129,11 @@ NSManagedObjectContext *managedObjectContext() {
     NSPersistentStoreCoordinator *coordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel: managedObjectModel()];
     [context setPersistentStoreCoordinator: coordinator];
     
-    NSString *STORE_TYPE = NSSQLiteStoreType;
+    NSError* error;
+    NSPersistentStore* newStore = [coordinator addPersistentStoreWithType:g_storeType configuration:nil URL:g_dbURL options:nil error:&error];
     
-    NSString *path = [[[NSProcessInfo processInfo] arguments] objectAtIndex:0];
-    path = [path stringByDeletingPathExtension];
-    NSURL *url = [NSURL fileURLWithPath:[path stringByAppendingPathExtension:@"sqlite"]];
-    
-    NSError *error;
-    NSPersistentStore *newStore = [coordinator addPersistentStoreWithType:STORE_TYPE configuration:nil URL:url options:nil error:&error];
-    
-    if (newStore == nil) {
+    if (newStore == nil)
+    {
         NSLog(@"Store Configuration Failure %@",
               ([error localizedDescription] != nil) ?
               [error localizedDescription] : @"Unknown Error");
